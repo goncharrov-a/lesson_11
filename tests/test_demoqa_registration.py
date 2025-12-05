1492fcc15bf8efaf2528b3a486c067b44469459d
Goncharov Anton <goncharrov.a@icloud.com>
2025-12-05 11:24:35 +0300

    upd test -> pom

--- tests/test_demoqa_registration.py
+++ tests/test_demoqa_registration.py
@@ -1,65 +1,80 @@
-from pathlib import Path
+import allure
+import pytest
+from selene import browser
 
+from data.demoqa_user_data import users
 from pages.practice_form_page import PracticeFormPage
-
-FIRST_NAME = 'Luke'
-LAST_NAME = 'Skywalker'
-EMAIL = 'example.user@mail.ru'
-GENDER = 'Male'
-PHONE = '9990003388'
-
-BIRTH_DAY = '15'
-BIRTH_MONTH = 'January'
-BIRTH_YEAR = '1992'
-
-SUBJECT_1 = 'Maths'
-SUBJECT_2 = 'Computer Science'
-
-HOBBY_1 = 'Sports'
-HOBBY_2 = 'Reading'
-HOBBY_3 = 'Music'
-
-ADDRESS = 'Tatooine home'
-STATE = 'NCR'
-CITY = 'Delhi'
-
-FILE = Path(__file__).parent / 'resources' / 'image.png'
-FILE_NAME = FILE.name
-
-EXPECTED_DOB = f'{BIRTH_DAY} {BIRTH_MONTH},{BIRTH_YEAR}'
-EXPECTED_STATE_CITY = f'{STATE} {CITY}'
+from utils import attach
 
 
-def test_demo_qa_pom():
-    page = PracticeFormPage().open()
+@allure.epic("DemoQA")
+@allure.feature("Practice Form")
+@allure.story("Генерация пользователя через Faker и проверка заполнения формы")
+@allure.tag("UI", "PracticeForm", "Faker", "Smoke")
+@allure.severity(allure.severity_level.CRITICAL)
+@pytest.mark.parametrize(
+    "user",
+    users,
+    ids=lambda u: f"{u.first_name}_{u.last_name}",
+)
+def test_practice_form(setup_browser, user):
+    page = PracticeFormPage()
 
-    page.fill_name(FIRST_NAME, LAST_NAME) \
-        .set_email(EMAIL) \
-        .set_phone(PHONE) \
-        .set_dob(BIRTH_DAY, BIRTH_MONTH, BIRTH_YEAR) \
-        .set_gender(GENDER) \
-        .add_subject(SUBJECT_1) \
-        .add_subject(SUBJECT_2) \
-        .select_hobby(HOBBY_1) \
-        .select_hobby(HOBBY_2) \
-        .select_hobby(HOBBY_3) \
-        .upload_picture(str(FILE)) \
-        .set_address(ADDRESS) \
-        .select_state_city(STATE, CITY) \
-        .submit()
+    allure.dynamic.title(f"Заполнение Practice Form для {user.first_name} {user.last_name}")
+    allure.dynamic.description(
+        "Тест проверяет корректность заполнения Practice Form с использованием пользовательских "
+        "данных, сгенерированных через Faker."
+    )
 
-    page.should_see_modal() \
-        .should_have_row('Student Name', f'{FIRST_NAME} {LAST_NAME}') \
-        .should_have_row('Student Email', EMAIL) \
-        .should_have_row('Gender', GENDER) \
-        .should_have_row('Mobile', PHONE) \
-        .should_have_row('Date of Birth', BIRTH_DAY, BIRTH_MONTH, BIRTH_YEAR) \
-        .should_have_row('Subjects', SUBJECT_1) \
-        .should_have_row('Subjects', SUBJECT_2) \
-        .should_have_row('Hobbies', HOBBY_1) \
-        .should_have_row('Hobbies', HOBBY_2) \
-        .should_have_row('Hobbies', HOBBY_3) \
-        .should_have_row('Picture', FILE_NAME) \
-        .should_have_row('Address', ADDRESS.replace('\n', ' ')) \
-        .should_have_row('State and City', STATE, CITY) \
-        .close()
+    with allure.step("Открыть форму Practice Form"):
+        page.open()
+
+    with allure.step("Заполнить имя и фамилию"):
+        page.fill_name(user.first_name, user.last_name)
+
+    with allure.step("Заполнить email"):
+        page.set_email(user.email)
+
+    with allure.step("Заполнить номер телефона"):
+        page.set_phone(user.phone)
+
+    with allure.step("Выбрать пол"):
+        page.set_gender(user.gender)
+
+    with allure.step("Указать дату рождения"):
+        page.set_dob(
+            user.birth_day,
+            user.birth_month,
+            user.birth_year,
+        )
+
+    with allure.step("Добавить предметы"):
+        for subject in user.subjects:
+            page.add_subject(subject)
+
+    with allure.step("Выбрать хобби"):
+        for hobby in user.hobbies:
+            page.select_hobby(hobby)
+
+    with allure.step("Загрузить файл"):
+        page.upload_picture(str(user.file_path))
+
+    with allure.step("Заполнить адрес"):
+        page.set_address(user.address)
+
+    with allure.step("Выбрать штат и город"):
+        page.select_state_city(user.state, user.city)
+
+    with allure.step("Отправить форму"):
+        page.submit()
+
+    with allure.step("Проверить модальное окно и данные"):
+        page.should_see_modal() \
+            .should_have_row("Student Name", f"{user.first_name} {user.last_name}") \
+            .should_have_row("Student Email", user.email) \
+            .should_have_row("Gender", user.gender)
+
+    with allure.step("Добавить вложения"):
+        attach.add_html(browser)
+        attach.add_screenshot(browser)
+        attach.add_logs(browser)
